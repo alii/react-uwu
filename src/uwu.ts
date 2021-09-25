5919cbf5e0398c0f93d45f276748a1908ed8b3c9
Bereket Semagn <semagnbereket@gmail.com>
2021-09-24 22:57:28 -0400

    fixed

--- src/uwu.ts
+++ src/uwu.ts
@@ -2,20 +2,19 @@ import {kaomoji, kaomojiConfused} from './kaomoji';
 
 export const uwu = (text: string) => {
 	const noMatch = Math.random() < 0.25;
-	let input = '';
 
 	// Find Regex Matches
-	input = text.replace(/(?:l|r)/g, 'w');
-	input = text.replace(/(?:L|R)/g, 'W');
-	input = input.replace(/!+/g, `${kaomoji[Math.floor(Math.random() * kaomoji.length)]}`);
-	input = input.replace(
-		/?+/g,
+	text = text.replace(/(?:l|r)/g, 'w');
+    text = text.replace(/(?:L|R)/g, 'W');
+	text = text.replace(/!+/g, ` ${kaomoji[Math.floor(Math.random() * kaomoji.length)]}`);
+	text = text.replace(
+		new RegExp("\\?", "g"),
 		`${kaomojiConfused[Math.floor(Math.random() * kaomojiConfused.length)]}`
 	);
 
 	if (noMatch) {
-		input = `${input.charAt(0)}-${input}`;
+		text = `${text.charAt(0)}-${text}`;
 	}
 
-	return input;
+	return text;
 };
